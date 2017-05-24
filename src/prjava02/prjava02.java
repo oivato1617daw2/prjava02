001fc9cc93f8f54d6a9c49ec6e97fb1becca18ef
oihane <oivato1617daw2@gmail.com>
2017-05-24 12:21:45 +0200

    Treballant amb ordres Git i la branca01 v0.1

--- src/prjava02/prjava02.java
+++ src/prjava02/prjava02.java
@@ -5,32 +5,35 @@ package prjava02;
  */
 
 import java.io.*;
-import java.net.*; 
+import java.net.*;
+
 /**
  *
  * @author ¬¬> Escriu el teu correu electrònic
  */
 public class prjava02 {
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("versió 0.1 del projecte prjava02"); 
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("versió 0.1 del projecte prjava02");
 
-        System.out.println("Creació d'una branca del projecte prjava02");
+		System.out.println("Creació d'una branca del projecte prjava02");
 
-        System.out.println("Afegint més codi a la branca00 del projecte prjava02");
+		System.out.println("Afegint més codi a la branca00 del projecte prjava02");
 
-        try {
-            InetAddress adreça = InetAddress.getLocalHost();
-            String hostname = adreça.getHostName();
-            System.out.println("hostname="+hostname);
-            System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
-            System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
-            System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
-            System.out.println("Versió  OS: " + System.getProperty("os.version"));
-        }
-        catch (IOException e) {
-        }  
-    }
+		System.out.println("Afegint codi a la branca01 del projecte prjava02");
+
+		try {
+			InetAddress adreça = InetAddress.getLocalHost();
+			String hostname = adreça.getHostName();
+			System.out.println("hostname=" + hostname);
+			System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
+			System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
+			System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
+			System.out.println("Versió  OS: " + System.getProperty("os.version"));
+		} catch (IOException e) {
+		}
+	}
 }
